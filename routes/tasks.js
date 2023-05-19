012dbb8a01d084ce6fb97d0d13d55220bd46ca70
Ike-Ikon <asieduisaac5775@gmail.com>
2023-05-19 22:44:46 +0000

    using GraphQL

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,15 +1,50 @@
 const express = require('express');
-const router = express.Router();
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const tasksData = require('../controllers/tasks');
 
-router.get('/', tasksData.getAllTasks);
+// Define your GraphQL schema
+const schema = buildSchema(`
+  type Task {
+    id: ID!
+    title: String!
+    assignedTo: String!
+    description: String
+    status: String
+    priority: String
+    deadline: String
+    additionalFields: String
+  }
 
-// router.get('/:id', tasksData.getSingleUser);
+  type Query {
+    getAllTasks: [Task]
+  }
 
-router.post('/', tasksData.createTask);
+  type Mutation {
+    createTask(title: String!, assignedTo: String!): Task
+    deleteTask(assignedTo: String!): Boolean
+  }
+`);
 
-router.delete('/:assignedTo', tasksData.deleteTask);
+// Define your resolver functions
+const root = {
+  getAllTasks: tasksData.getAllTasks,
+  createTask: tasksData.createTask,
+  deleteTask: tasksData.deleteTask
+};
 
+// Create the Express router
+const router = express.Router();
+
+// Set up the GraphQL endpoint
+router.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true // Enable GraphiQL for testing the API
+  })
+);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,51 @@
 const express = require('express');
-const router = express.Router();
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const usersData = require('../controllers/users');
 
-router.get('/', usersData.getAllUsers);
+// Define your GraphQL schema
+const schema = buildSchema(`
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    password: String!
+    role: String!
+    department: String!
+    joinDate: String!
+    contactNumber: String
+    profilePicture: String
+  }
 
-router.get('/:name', usersData.getSingleUser);
+  type Query {
+    getAllUsers: [User]
+    getSingleUser(name: String!): User
+  }
 
-router.post('/', usersData.createUser);
+  type Mutation {
+    createUser(name: String!, email: String!, password: String!, role: String!, department: String!, joinDate: String!, contactNumber: String, profilePicture: String): User
+  }
+`);
+
+// Define your resolver functions
+const root = {
+  getAllUsers: usersData.getAllUsers,
+  getSingleUser: usersData.getSingleUser,
+  createUser: usersData.createUser
+};
+
+// Create the Express router
+const router = express.Router();
+
+// Set up the GraphQL endpoint
+router.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true // Enable GraphiQL for testing the API
+  })
+);
 
 module.exports = router;
