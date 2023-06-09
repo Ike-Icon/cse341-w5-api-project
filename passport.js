3b87bae6b8c03d19d1bf85afacaac17526ee1b55
Ike-Ikon <asieduisaac5775@gmail.com>
2023-06-09 23:29:10 +0000

    passport to create user

--- passport.js
+++ passport.js
@@ -29,36 +29,25 @@ passport.use(
           throw new Error('Invalid profile data');
         }
 
-        // Check if the user exists in the database
-        const existingUser = await mongodb
+        // Create a new user
+        const newUser = {
+          googleId: profile.id,
+          name: profile.displayName,
+          email: profile.emails[0].value
+          // Additional user data
+        };
+
+        // Insert the new user into the database
+        const response = await mongodb
           .getDb()
           .db('task_mgt_sys')
           .collection('users')
-          .findOne({ googleId: profile.id });
+          .insertOne(newUser);
 
-        if (existingUser) {
-          // User already exists, return the user
-          done(null, existingUser);
+        if (response.acknowledged) {
+          done(null, newUser);
         } else {
-          // Create a new user
-          const newUser = {
-            googleId: profile.id,
-            name: profile.displayName,
-            email: profile.emails[0].value
-            // Additional user data
-          };
-
-          const response = await mongodb
-            .getDb()
-            .db('task_mgt_sys')
-            .collection('users')
-            .insertOne(newUser);
-
-          if (response.acknowledged) {
-            done(null, newUser);
-          } else {
-            done(new Error('Failed to create a new user'));
-          }
+          done(new Error('Failed to create a new user'));
         }
       } catch (error) {
         done(error);
