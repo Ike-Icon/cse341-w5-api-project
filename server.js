e628ab5d7c3705ca362317874c045640cf3947d2
Ike-Ikon <asieduisaac5775@gmail.com>
2023-06-10 22:05:09 +0000

    server edited

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
-const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-const session = require('express-session');
 const app = express();
-const exphbs = require('express-handlebars');
 const mongodb = require('./db/connect');
 const port = process.env.PORT || 3000;
 
@@ -18,25 +15,12 @@ app
     next();
   })
 
-  // Set the view engine
-  .engine('.hbs', exphbs.create({ defaultLayout: 'main', extname: '.hbs' }).engine)
-  .set('view engine', '.hbs')
 
-  // Sessions
-  .use(
-    session({
-      secret: 'keyboard cat',
-      resave: false,
-      saveUninitialized: false
-    })
-  )
 
   // Passport middleware
   .use(passport.initialize())
   .use(passport.session())
 
-  // Static folder
-  .use(express.static(path.join(__dirname, 'public')))
 
   // Routes
   .use('/', require('./routes'));
